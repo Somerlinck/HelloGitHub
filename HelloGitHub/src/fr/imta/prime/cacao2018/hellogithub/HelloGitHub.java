45013739fe7952fba907f2611912a7db5d9ffba4
Utilisateur <Utilisateur@DESKTOP-KCC3NM7>
2018-04-06 17:50:51 +0200

    Fonction

--- HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/HelloGitHub.java
+++ HelloGitHub/src/fr/imta/prime/cacao2018/hellogithub/HelloGitHub.java
@@ -1,9 +1,11 @@
 package fr.imta.prime.cacao2018.hellogithub;
-
 public class HelloGitHub {
-
+	public static void pseudo() {
+		System.out.println("elisagm");
+	}
 	public static void main(String[] args) {
-
+		pseudo();
+			
 	}
 
 }
